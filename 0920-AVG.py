0d76713d0e526d4939ad06ca41f6c84af34ea070
young03281 <young03281@gmail.com>
2024-09-28 14:59:58 +0800

    merge this

--- 0920-AVG.py
+++ 0920-AVG.py
@@ -3,18 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
-    
 
 matplotlib.rc('font', family='Microsoft YaHei')
-
-N, M, NUM = 3, 5, 41
         
 # 讀取 Excel 檔案中的資料
 df = pd.read_csv('formed.csv')
 
 df.drop(df.columns[0], axis=1, inplace = True)
 
-group = [[] for i in range(3)]
+group = [[[]]for i in range(3)] 
 for i in df:
     n = 0
     for j in df[i]:
@@ -29,15 +26,14 @@ for i in df:
         else:
             group[2][n].append(j)
         n+=1
-        
+
 def uC(samples:list[list]):
     sum = 0
     pow_ub = 0.01/12 # pow(0.1/2/sqrt(3)) = 0.01 / (4*3)
     for i in samples:
-        sum += pow(np.std(i, ddof=1), 2)/n + pow_ub
+        sum += pow(np.std(i, ddof=1), 2)/len(i) + pow_ub
+    print(sum)
     return np.sqrt(sum)
-    
-    
 
 X = ["對照組", "99乘法組", "拍肩舉手組"]
 Y = [np.average(i) for i in group]
