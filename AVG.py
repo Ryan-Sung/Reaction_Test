34125c1b0d1a21d5dac12eace16287ff40192f4b
gdd0142 <ryansung0910@gmail.com>
2024-09-28 17:21:29 +0800

    prob the final version ==
    花了一天又重寫一遍，累

--- AVG.py
+++ AVG.py
@@ -2,61 +2,72 @@ import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import math
+import platform
 
-matplotlib.rc('font', family='Apple LiSung')
+matplotlib.rc('font', family=['Microsoft YaHei', 'Apple LiSung'][platform.system()=="Darwin"])
 
-N, M, NUM, PRECISION = 3, 5, 41, 3
+'''
+N = group number
+M = times per tester do per group
+NUM = number of testers
+PRECISION = float precision
+LEAST_COUNT = 0.1 (cm)
+'''
+N, M, NUM, PRECISION, LEAST_COUNT = 3, 5, 41, 3, 0.1
+
+def sqrt(x):
+    return x**0.5
 
 class DATA:
-    def __init__(self, N:int, M:int, argv:list):
-        self.X =[[argv[i*M + j] for j in range(M)] for i in range(N)]
+    def __init__(self, sz:int, argv:list):
+        self.X = argv
+        self.sz = sz
+        self.SUM = sum(self.X)
+        self.AVG = self.SUM/self.sz
         
-        self.AVG = [sum(X)/M for X in self.X]
+        # ddof = 0/1 -> population/sample deviation
+        self.STDEV = np.std(self.X, ddof = 1) 
         
-        # ddof = 1 -> sample deviation not population one
-        self.STDEV = [np.std(X, ddof=1) for X in self.X]
-        
-        # type A/B uncertainty
-        self.VA = [ stdev / (M**0.5) for stdev in self.STDEV ]
-        self.VB = [0.1 / ( 2 * ( 3 ** 0.5 ) )] * N
-        
-        # combination uncertainty
-        self.V = [( (va**2) + (vb**2) ) for va, vb in zip(self.VA, self.VB) ]
-        
-        
+        # Uncertainty A/B/Combination
+        self.UA = self.STDEV/sqrt(sz)
+        self.UB = LEAST_COUNT/2/sqrt(3)
+        self.UC = sqrt(self.UA**2 + self.UB**2)
+    
     def print(self):
-        print("AVG =", self.AVG)
-        print("STDEV =", self.STDEV)
-        print("VA =", self.VA)
-        print("V =", self.V)
-        
-        
-# 讀取 Excel 檔案中的資料
+        print("list =", list(self.X))
+        print("sum =", self.SUM)
+        print("average =", self.AVG)
+        print("stdev =", self.STDEV)
+        print("uncertainty =", self.UC)
+
 df = pd.read_csv('formed.csv')
-
-df.drop(df.columns[0], axis=1, inplace = True)
-
-for i in range(15):
+df.drop('num', axis=1, inplace = True)
+for i in range(N*M):
     df[i] = df[str(i)] # df[1] = df['1']
     df.drop(str(i), axis=1, inplace=True)
-    
-    
-data_by_num = [DATA(N, M, df.iloc[[num]]) for num in range(NUM)]
 
-data_by_group = DATA(N, M*NUM, list([df[i*M + j][k] for i in range(N) for j in range(M) for k in range(NUM)]))
 
-V = [ (sum([data_by_num[j].V[i] for j in range(NUM)])) ** 0.5 for i in range(N)]
-V = [ round(x, PRECISION) for x in V ]
+# data_by_num[the number of tester][group_num]
+data_by_num = [[DATA(M, df.iloc[num, i*M:(i+1)*M]) for i in range(N)] for num in range(NUM)]
 
-X = ["對照組", "99乘法組", "拍肩舉手組"]
-Y = data_by_group.AVG
+# AVG = [A0, A1, A2]
+# which Ai = sum(data_by_num[for all num][i].SUM) / (M*NUM)
+AVG = [sum(data_by_num[num][i].SUM for num in range(NUM)) / (M*NUM)  for i in range(N)]
 
-plt.bar(X, Y, yerr = V, ecolor = '#00BB00')
+# uncertainty
+# U = [U0, U1, U2]
+# which Ui = sqrt( sum(data_by_num[for all num][i].UC ** 2) )
+U = [sqrt(sum(data_by_num[num][i].UC**2 for num in range(NUM))) for i in range(N)]
+U = [round(x, PRECISION) for x in U]
 
+
+X, Y = ["對照組", "99乘法組", "拍肩舉手組"], AVG
+plt.bar(X, Y, yerr = U, ecolor = '#00BB00')
 plt.title("三種變因的接尺實驗數據長條圖 - 第四組", fontsize = 20)
 plt.xlabel("組別", fontsize = 15)
 plt.ylabel("接到尺的距離 (cm)", fontsize = 15)
-plt.text(-0.2, 30, fr'$\mu_A={V[0]}$' + '\n' + fr'$\mu_B={V[1]}$' + '\n' + fr'$\mu_C={V[2]}$', color = '#00BB00', fontsize = 20)
-plt.errorbar(X, Y, yerr=V, ecolor='#00BB00', elinewidth=2, capsize=4, fmt=' ')
+plt.text(-0.2, 30, fr'$\mu_A={U[0]}$' + '\n' + fr'$\mu_B={U[1]}$' + '\n' + fr'$\mu_C={U[2]}$', color = '#00BB00', fontsize = 20)
+plt.errorbar(X, Y, yerr=U, ecolor='#00BB00', elinewidth=2, capsize=4, fmt=' ')
 
 plt.show()
